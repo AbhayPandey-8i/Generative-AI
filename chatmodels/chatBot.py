eaa944175fc11e334a1743ad6b36a24b395dca35
Abhay Pandey <pandeyabhay055@gmail.com>
2026-09-04 23:50:03 +0530

    updated

--- chatmodels/chatBot.py
+++ chatmodels/chatBot.py
@@ -3,22 +3,25 @@ from dotenv import load_dotenv
 load_dotenv() 
 
 from langchain_mistralai import ChatMistralAI
+from langchain_core.messages import AIMessage, SystemMessage, HumanMessage #to add distinct roles in our history;
+
+
 
 model = ChatMistralAI(model = "mistral-small-2603", temperature=0.9)
 
 #all prompts and responses will be here in history, so that bot can have context and memory to previous chat
 history = [
-
+     SystemMessage(content = "You are a Rude AI agent")
 ]
 
 print("----------------Welcome to AI chatbot, Press 0 to exit--------------------")
 while True:
     prompt = input("You: ")
-    history.append(prompt)
+    history.append(HumanMessage(content = prompt))
     if(prompt == "0"):
         print("Exit Successfully")
         break
 
     response = model.invoke(history)
-    history.append(response)
+    history.append(AIMessage(content = response.content))
     print("Bot: ",response.content) 
